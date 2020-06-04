f6ac849e1bfa3d881530511197ef5d8d4814d00a
Kanta Munton <kanta.munton@gmail.com>
2020-06-04 14:13:21 +0100

    static files added and settings updated

--- mysouschef/settings.py
+++ mysouschef/settings.py
@@ -133,3 +133,11 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
