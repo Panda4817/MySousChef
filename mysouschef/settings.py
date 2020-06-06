ab7143c136dd4005409a7460e11200d05a1a7688
Kanta Munton <kanta.munton@gmail.com>
2020-06-06 10:10:30 +0100

    fixed allowed hosts and testinf auto deploy from travis

--- mysouschef/settings.py
+++ mysouschef/settings.py
@@ -27,9 +27,9 @@ load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'mysouschef.herokuapp.com']
 
 
 # Application definition
