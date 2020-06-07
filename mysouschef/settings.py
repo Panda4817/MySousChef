fd538fbbfee21f12a93ff35a87b92a2d69f21c3c
Kanta Munton <kanta.munton@gmail.com>
2020-06-07 12:45:52 +0100

    changes made to security settings

--- mysouschef/settings.py
+++ mysouschef/settings.py
@@ -30,7 +30,14 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'mysouschef.herokuapp.com']
+
+# Security settings
 SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 3600
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+
 # Application definition
 
 INSTALLED_APPS = [
