8b04069d4a234ef08d230ecb24c2935f36b735a3
Kanta Munton <kanta.munton@gmail.com>
2020-06-07 12:20:26 +0100

    changed logo size

--- mysouschef/settings.py
+++ mysouschef/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'mysouschef.herokuapp.com']
-
+SECURE_SSL_REDIRECT = True
 # Application definition
 
 INSTALLED_APPS = [
